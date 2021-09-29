ea93baddad1b20bdd7ef9c4566bd366bcaf22d23
Mikhail <stroryz@gmail.com>
2021-09-29 17:10:47 +0300

    changed 29.09.21

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log('global');
+"use strict";
+
+console.log('maxgraph');
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+"use strict";
+//# sourceMappingURL=main.js.map
