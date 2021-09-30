c90c712b8538055a55f276594152b6ce358a38c0
Mikhail <stroryz@gmail.com>
2021-09-30 16:01:31 +0300

    changed or added

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,41 @@ console.log('global');
 "use strict";
 
 console.log('maxgraph');
+"use strict";
+
+var rangeSlider = document.getElementById('range-slider');
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [500, 999999],
+    connect: true,
+    step: 1,
+    range: {
+      'min': [500],
+      'max': [999999]
+    }
+  });
+  var input0 = document.getElementById('input-0');
+  var input1 = document.getElementById('input-1');
+  var inputs = [input0, input1];
+  rangeSlider.noUiSlider.on('update', function (values, handle) {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  var setRangeSlider = function setRangeSlider(i, value) {
+    var arr = [null, null];
+    arr[i] = value;
+    console.log(arr);
+    rangeSlider.noUiSlider.set(arr);
+  };
+
+  inputs.forEach(function (el, index) {
+    el.addEventListener('change', function (e) {
+      console.log(index);
+      setRangeSlider(index, e.currentTarget.value);
+    });
+  });
+}
 /**
   * название функции
   *
