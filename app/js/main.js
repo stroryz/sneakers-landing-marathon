9139b005409efe2e30304a6cfa5727e4b0a445ce
Mikhail <stroryz@gmail.com>
2021-11-17 11:03:24 +0300

    modified

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,14 @@ accordions.forEach(function (el) {
     }
   });
 });
+// const productBtns = document.querySelectorAll('.product__btn');
+// productBtns.forEach(el => {
+//   el.addEventListener('focus', (e) => {
+//     let parent = e.currentTarget.closest('.product__btns');
+//     parent.classList.add('.product__btns--active');
+//   })
+// })
+"use strict";
 // const insta = document.querySelector('.instagram__content');
 //   (function () {
 //     new InstagramFeed({
---
+++ src/js/components/foucs.js
@@ -0,0 +1,8 @@
+// const productBtns = document.querySelectorAll('.product__btn');
+
+// productBtns.forEach(el => {
+//   el.addEventListener('focus', (e) => {
+//     let parent = e.currentTarget.closest('.product__btns');
+//     parent.classList.add('.product__btns--active');
+//   })
+// })
